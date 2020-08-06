4f57533c2e814f485caf31aece8bc234ae5eb4c5
Magu662 <69323825+Magu662@users.noreply.github.com>
2020-08-06 22:06:55 +0200

    Update action_rec.py

--- action_rec.py
+++ action_rec.py
@@ -17,3 +17,23 @@ def action():
         led.value = 0.25
         vibro.value = 0
         sleep(0.2)
+        led.value = 1
+        vibro.value = 0.33
+        sleep(1)
+        led.value = 0.25
+        vibro.value = 0
+    for i in range(2):
+        led.value = 1
+        sleep(0.2)
+        led.value = 0.25
+        sleep(1)
+        led.value = 1
+        sleep(0.2)
+        led.value = 0.25
+        sleep(1)
+    led.value = 0
+while True:
+        button.when_pressed = action
+
+
+
