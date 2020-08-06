d187cda01623ca9fb7279765c9bb601d48819731
Magu662 <69323825+Magu662@users.noreply.github.com>
2020-08-06 22:02:07 +0200

    Update mail_rec.py

--- mail_rec.py
+++ mail_rec.py
@@ -8,8 +8,8 @@ MAIL_CHECK_FREQ = 15        # check mail every 60 seconds
 
 # The following three variables must be customized for this
 # script to work
-USERNAME = 'maxs.raspi@gmail.com'
-PASSWORD = 'MaxsPi2020'
+USERNAME = 'test@gmail '
+PASSWORD = 'password'
 
 
 
@@ -17,3 +17,22 @@ def mail_check():
     # login to mailserver
     server = IMAPClient(HOSTNAME, use_uid=True, ssl=True)
     server.login(USERNAME, PASSWORD)
+    
+    # select our MAILBOX and looked for unread messages
+    unseen = server.folder_status(MAILBOX, ['UNSEEN'])
+
+    # number of unread messages
+    # print to console to determine NEWMAIL_OFFSET
+    newmail_count = (unseen[b'UNSEEN'])
+    print('%d unseen messages' % newmail_count)
+
+    if newmail_count > 0:
+
+        print ('recived')
+       # os.system("cd /home/pi/del.py")
+    time.sleep(MAIL_CHECK_FREQ)
+
+while True:
+    mail_check()
+
+
